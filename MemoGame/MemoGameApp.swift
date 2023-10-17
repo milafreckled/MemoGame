74929f2a2c7f4453034e74ba9126dd3ecb1a27e5
milafreckled <miladul2014@gmail.com>
2023-10-17 14:34:10 +0200

    Initial Commit

---
+++ MemoGame/MemoGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoGameApp.swift
+//  MemoGame
+//
+//  Created by student on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
